b9dd447970385aa236efc5517c0ffb7d77356cdf
Mikkel Mondrup Kristensen <mikkel@tdx.dk>
2024-09-21 17:28:02 +0200

    Revert "Work with git worktrees"
    This reverts commit 8a14329c3dd32883f367e59f50167aeb5faa3e62.

--- lua/arrow/save_keys.lua
+++ lua/arrow/save_keys.lua
@@ -1,24 +1,17 @@
 local M = {}
 
 function M.cwd()
-  return vim.loop.cwd()
+	return vim.loop.cwd()
 end
 
 function M.git_root()
-  local git_worktree = vim.fn.system "git rev-parse --is-inside-work-tree 2>&1"
-  local git_root
+	local git_root = vim.fn.system("git rev-parse --show-toplevel 2>&1")
 
-  if git_worktree then
-    git_root = vim.fn.system "git rev-parse --path-format=absolute --git-common-dir 2>&1"
-  else
-    git_root = vim.fn.system "git rev-parse --show-toplevel 2>&1"
-  end
+	if vim.v.shell_error == 0 then
+		return git_root:gsub("\n$", "")
+	end
 
-  if vim.v.shell_error == 0 then
-    return git_root:gsub("\n$", "")
-  end
-
-  return M.cwd()
+	return M.cwd()
 end
 
 return M
